0f78f73049e340eec7b67af49eb4e6f7be255633
mldkyt <mldkyt@proton.me>
2024-02-23 19:32:23 +0100

    Initial commit

---
+++ Assets/Building/Build.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class Build {
+
+	[MenuItem("Building/Build Asset Bundles")]
+	public static void BuildAssetBundles()
+	{
+		var path = Path.Combine(Application.dataPath, "Building/AssetBundles");
+		if (!Directory.Exists(path))
+		{
+			Directory.CreateDirectory(path);
+		}
+		BuildPipeline.BuildAssetBundles(path);
+	}
+}
